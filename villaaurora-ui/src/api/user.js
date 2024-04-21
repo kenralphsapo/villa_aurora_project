7e28f8ef1efc252f8704a2fade6b4b2c02eca9af
William Adornado <adornadowilliam@gmail.com>
2024-04-21 19:39:13 +0800

    Update user.js, Admin.jsx, .Home.jsx

--- villaaurora-ui/src/api/user.js
+++ villaaurora-ui/src/api/user.js
@@ -13,31 +13,6 @@ export const index = async (token) => {
 }
 
 export const store = async (body, token) => {
-    const response = await fetch(`${url}/users`, {
-        method: 'POST',
-        headers: {
-          Accept: "application/json",
-"Content-Type": 'application/json'
-          Authorization: `Bearer ${token}`
-        },
-body: JSON.stringify(body)
-      })
-
-      return await response.json()
-}
-
-  const response = await fetch(`${url}/users`, {
-    method: 'GET',
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`
-    },
-  });
-
-  return await response.json();
-};
-
-export const store = async (body, token) => {
   const response = await fetch(`${url}/users`, {
     method: 'POST',
     headers: {
