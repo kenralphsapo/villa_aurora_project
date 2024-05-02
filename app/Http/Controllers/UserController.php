c29b539c2aea1ab3cb767fafffb3e6afab71ce61
Adornadowilliam2 <145187649+Adornadowilliam2@users.noreply.github.com>
2024-05-03 06:45:26 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -106,9 +106,9 @@ public function show(Request $request, User $user){
         "role" => "sometimes|in:guest,scheduler,admin"
     ]);
 
-    // if ($request->user()->role !== "admin") {
-    //     unset($validator['role']);
-    // }
+    if ($request->user()->role !== "admin") {
+        unset($validator['role']);
+    }
 
     // if ($request->user()->role !== "admin") {
     //     $validator->sometimes('role', 'required|in:guest,scheduler,admin', function ($input) {
