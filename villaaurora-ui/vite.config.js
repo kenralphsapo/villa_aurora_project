9be46dbd2a1c9714b35cd14ff4e2799582b0c0f9
William Adornado <adornadowilliam@gmail.com>
2024-05-12 11:30:56 +0800

    adding security features like https in react js

--- villaaurora-ui/vite.config.js
+++ villaaurora-ui/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
+  server: {
+    https: {
+      key:fs.readFileSync("./localhost.key"),
+      cert:fs.readFileSync("./localhost.crt")
+    }
+    
+  }
+
 })
